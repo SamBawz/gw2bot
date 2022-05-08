ed36a3205f2cfedf867dff9079548368fb1f98f9
SamBawz <sambosuitstein@hotmail.com>
2022-05-08 15:09:15 +0200

    Update index.js
    Vervang de timer met een event dat afgevuurd wordt wanneer mensen online komen. Op deze manier worden er geen berichten gestuurd wanneer gebruikers niet online zijn. Voor nu logt deze event alleen in de console zodat ik kan bijhouden hoe goed dit werkt. Verder is er een !fractal command toegevoegd om een fractal event aan te maken.

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
 const Discord = require('discord.js');
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message} = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 //const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const client = new Discord.Client({
-    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
+    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_PRESENCES]
 });
 let talkedRecently = new Set();
 const prefix = "!";
 //Required to fetch api data
 const fetch = require('node-fetch');
 
-//let targetChannel = "";
+let targetChannel = "";
 client.once("ready", () => {
     console.log("bot up");
     //bottest channel
     //client.channels.fetch('444237157029380096').then(targetChannel => channel = targetChannel);
     //Fractal channel
-    //client.channels.fetch('750798181687885954').then(channel => targetChannel = channel);
+    client.channels.fetch('750798181687885954').then(channel => targetChannel = channel);
     //checkDailies();
     //console.log("bot started on " + date.getHours() + " hours.");
     //setInterval(checkTime, 36000);
+    client.user.setActivity("Type " + prefix + "fractals to start an event!", {type: "WATCHING"});
+});
+
+client.on("presenceUpdate", () => {
+    checkTime();
 });
 
 client.on("messageCreate", (message) => {
-    console.log(message);
-
-    //If the author is a bot then ignore
-    if (message.author.bot) {
-        return;
-    }
-
-    //If talkedRecently still exists (the bot has been summoned more then once in 1.5 seconds and by the same user) then return
-    if (talkedRecently.has(message.author.id)) {
-        return;
-    }
-
-    //Adds the user to the talkedRecently set
-    talkedRecently.add(message.author.id);
-    setTimeout(() => {
-        //Removes the user from the talkedRecently set after 1 second
-        talkedRecently.delete(message.author.id);
-    }, 1000);
-
     //If the message starts with the prefix
     if (message.content.startsWith(prefix)) {
+        //If the author is a bot then ignore
+        if (message.author.bot) {
+            return;
+        }
+
+        //If talkedRecently still exists (the bot has been summoned more then once in 1.5 seconds and by the same user) then return
+        if (talkedRecently.has(message.author.id)) {
+            return;
+        }
+
+        //Adds the user to the talkedRecently set
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+            //Removes the user from the talkedRecently set after 1 second
+            talkedRecently.delete(message.author.id);
+        }, 1000);
+
+        //Command processing
         let args = message.content.slice(prefix.length).trim().split(/ +/g);
         let command = args.shift().toLowerCase();
-        console.log(command);
         switch (command) {
             case "fractals" :
-                checkDailies(message.channel);
-                message.delete();
+                checkDailies(message.channel, message.author.username);
+                //message.delete();
                 break;
         }
     }
-}); 
+});
 
-const date = new Date();
-let checkCooldown = false;
+const day = 1000 * 60 * 60 * 24;
+let lastCheck = 0;
 function checkTime() {
-    if (date.getHours() === 5 && !checkCooldown) {
-        checkDailies();
-        checkCooldown = true;
-    }
-    else if (date.getHours() > 5) {
-        checkCooldown = false;
+    if (lastCheck !== Math.ceil(Date.now() / day)) {
+        //checkDailies(targetChannel);
+        console.log("new day!");
+        lastCheck = Math.ceil(Date.now() / day);
     }
 }
 
-function checkDailies(channel) {
+function checkDailies(channel, creator) {
     let returnMessage = "Todays T4 fractal dailies are:"
     let dailyCount = 0;
 
@@ -102,14 +103,27 @@ function checkDailies(channel) {
     //Delay because fetching the data uses async functions
     setTimeout(() => {
         if (channel && channel.isText()) {
+            if (creator.length > 0) {
+                let fractalEmbed = new MessageEmbed()
+                    .setColor('#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6))
+                    .setTitle(creator + " has created a fractal event!")
+                    .setDescription(returnMessage + ".")
+                    .addField('\u200b', 'React with   :white_check_mark:   to join, or   :question:   if unsure.', false)
+                    .setTimestamp();
+                channel.send({ embeds: [fractalEmbed] });
+            }
+            else {
+                let fractalEmbed = new MessageEmbed()
+                    .setColor('#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6))
+                    .setTitle("Automated fractal event.")
+                    .setDescription(returnMessage + ".")
+                    .addField('\u200b', 'React with   :white_check_mark:   to join, or   :question:   if unsure.', false)
+                    .setTimestamp();
+                channel.send({ embeds: [fractalEmbed] });
+            }
 
-            let fractalEmbed = new MessageEmbed()
-                .setColor('#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6))
-                .setDescription(returnMessage)
-                .setTimestamp()
-                .setFooter({ text: "React with  :white_check_mark:  to join, or  :question:  if unsure" });
 
-            channel.send(fractalEmbed);
+
 
             //channel.send(returnMessage);
             //channel.send("React with  :white_check_mark:  to join, or  :question:  if unsure");
@@ -131,3 +145,4 @@ client.login(process.env.token);
 const config = require("./config.json");
 client.login(config.token);
  */
+
