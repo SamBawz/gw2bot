f91eec30ee38beec1f4bcc12a534fd5a12964b11
SamBawz <sambosuitstein@hotmail.com>
2022-06-01 13:49:41 +0200

    Schedule json file
    Schedule json file toegevoegd. Deze bevat alle informatie rond het rooster. Tegelijkertijd heb ik een nieuwe functie toegevoegd waarmee het rooster van de huidige week opgehaald en vertoond kan worden.

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const client = new Discord.Client({
     intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_PRESENCES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
 });
 let talkedRecently = new Set();
-const prefix = "!";
+
+let schedule = require("./schedule.json");
+let config = require("./config.json");
+const prefix = config.prefix;
 //Required to fetch api data
 const fetch = require('node-fetch');
 
@@ -14,13 +17,14 @@ let targetChannel = "";
 client.once("ready", () => {
     console.log("bot up");
     //bottest channel
-    //client.channels.fetch('444237157029380096').then(targetChannel => channel = targetChannel);
+    client.channels.fetch('444237157029380096').then(targetChannel => channel = targetChannel);
     //Fractal channel
-    client.channels.fetch('750798181687885954').then(channel => targetChannel = channel);
+    //client.channels.fetch('750798181687885954').then(channel => targetChannel = channel);
     //checkDailies();
     //console.log("bot started on " + date.getHours() + " hours.");
     //setInterval(checkTime, 36000);
     client.user.setActivity("Type " + prefix + "fractals to start an event!", {type: "WATCHING"});
+    postRaidSchedule(config.testChannel);
 });
 
 client.on("presenceUpdate", () => {
@@ -52,26 +56,72 @@ client.on("messageCreate", (message) => {
         let command = args.shift().toLowerCase();
         switch (command) {
             case "fractals" :
-                checkDailies(message.channel, message.author.username);
+                postFractalEvent(message.channel.id, message.author.username);
                 //message.delete();
                 break;
         }
     }
 });
 
-const day = 1000 * 60 * 60 * 24;
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
 //5 hours offset to account for the time at which reset happens in game
-const hourOffset = 1000 * 60 * 60 * 5;
-let lastCheck = Math.ceil((Date.now() - hourOffset) / day);
+const hourOffset = hour * 5;
+let lastDailyCheck = Math.ceil((Date.now() - hourOffset) / day);
+let lastWeeklyCheck = Math.ceil((Date.now() - hourOffset) / week);
 function checkTime() {
-    if (lastCheck !== Math.ceil((Date.now() - hourOffset) / day)) {
-        checkDailies(targetChannel);
-        console.log("new day! (day " + Math.ceil((Date.now() - hourOffset) / day) + ")");
-        lastCheck = Math.ceil((Date.now() - hourOffset) / day);
+    if (lastDailyCheck !== Math.ceil((Date.now() - hourOffset) / day)) {
+        postFractalEvent(config.testChannel);
+        //console.log("new day! (day " + Math.ceil((Date.now() - hourOffset) / day) + ")");
+        lastDailyCheck = Math.ceil((Date.now() - hourOffset) / day);
+    }
+    if (lastWeeklyCheck !== Math.ceil((Date.now() - hourOffset) / week)) {
+        postRaidSchedule(config.testChannel);
+        incrementCurrentWeek();
+        lastWeeklyCheck = Math.ceil((Date.now() - hourOffset) / week);
     }
 }
 
-function checkDailies(channel, creator) {
+function incrementCurrentWeek() {
+    schedule.currentWeek++;
+    if (schedule.currentWeek > schedule.MaxWeeks) {
+        schedule.currentWeek = 1;
+    }
+}
+
+function postRaidSchedule(channel) {
+    let wing1 = "";
+    let wing2 = "";
+    let wing3 = "";
+
+    //Find the current week in the json file and get the relevant wing names
+    schedule.weeks.forEach((week) => {
+        if (week.id === schedule.currentWeek) {
+            try {
+                wing1 = week['wings'][0].name;
+            } catch {}
+            try {
+                wing2 = ", " + week['wings'][1].name;
+            } catch {}
+            try {
+                wing3 = "and " + week['wings'][2].name;
+            } catch {}
+        }
+    });
+
+    let raidEmbed = new MessageEmbed()
+        .setColor('#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6))
+        .setTitle("Next week is week " + schedule.currentWeek + " of " + schedule.MaxWeeks + " (" + wing1 + wing2 + wing3 + ")")
+        .setDescription(schedule.generalMessage)
+        .addField('\u200b', 'And finally :no_entry_sign: if something urgent comes up and you will not be here!', false)
+        .setTimestamp();
+
+    client.channels.fetch(channel).then(targetChannel => targetChannel.send({ embeds: [raidEmbed] }));
+}
+
+function postFractalEvent(channel, creator) {
     let returnMessage = "Todays T4 fractal dailies are:"
     let dailyCount = 0;
 
@@ -112,12 +162,12 @@ function checkDailies(channel, creator) {
                     .setDescription(returnMessage + ".")
                     .addField('\u200b', 'React with   :white_check_mark:   to join, or   :question:   if unsure.', false)
                     .setTimestamp();
-                channel.send({ embeds: [fractalEmbed] }).then(msg => {
+                client.channels.fetch(channel).then(targetChannel => targetChannel.send({ embeds: [fractalEmbed] }).then(msg => {
                     msg.react('✅');
                     msg.react('❓');
                     //Delete after a day
                     setTimeout(() => msg.delete(), 86400000)
-                });
+                }));
             }
             else {
                 let fractalEmbed = new MessageEmbed()
@@ -126,12 +176,12 @@ function checkDailies(channel, creator) {
                     .setDescription(returnMessage + ".")
                     .addField('\u200b', 'React with   :white_check_mark:   to join, or   :question:   if unsure.', false)
                     .setTimestamp();
-                channel.send({ embeds: [fractalEmbed] }).then(msg => {
+                client.channels.fetch(channel).then(targetChannel => targetChannel.send({ embeds: [fractalEmbed] }).then(msg => {
                     msg.react('✅');
                     msg.react('❓');
                     //Delete after a day
                     setTimeout(() => msg.delete(), 86400000)
-                });
+                }));
             }
 
 
@@ -148,14 +198,12 @@ function checkDailies(channel, creator) {
 
 //Code to let the bot log in. This code has to be at the bottom
 //Login with heroku (live)
-client.login(process.env.token);
+//client.login(process.env.token);
 
 //LOGIN LINES FOR TESTING
 //Config contains the login token for the bot, the prefix and the owners id
 //This file will not be present in Heroku (is in .gitignore), so comment for live purposes
-/*
-const config = require("./config.json");
+
 client.login(config.token);
- */
 
 
